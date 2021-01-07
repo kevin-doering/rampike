ef3ed4c1eedb80d0f50c8ff3a184a8809539de34
Kevin Döring <k.doering.dt@gmail.com>
2021-01-07 15:29:00 +0100

    feat(workspace): extract static assets from app in a shared library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/rpk-client', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/rpk-client',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/shared/assets',
+  ],
 };
