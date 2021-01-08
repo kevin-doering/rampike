790c42546296b1c3dfd5d8ced57ee6a5b1b61bbe
Kevin Döring <k.doering.dt@gmail.com>
2021-01-08 23:02:12 +0100

    refactor(app): remove generated fragments

--- apps/rpk-client/src/app/app.component.ts
+++ apps/rpk-client/src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@rampike/shared/interfaces';
-import { environment } from '@rampike/shared/environments';
 
 @Component({
   selector: 'rampike-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  mode: string;
-  hello$ = this.http.get<Message>('http://localhost:3333/api/hello');
-  constructor(private http: HttpClient) {
-    this.mode = environment.production ? 'prod' : 'dev';
-  }
-}
+export class AppComponent {}
--- apps/rpk-client/src/app/app.module.ts
+++ apps/rpk-client/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule],
+  imports: [
+    RouterModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' })
+  ],
   providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
