e71fd23559268b4f1882c6a17acc3e5c5b0d333b
Kevin Döring <k.doering.dt@gmail.com>
2021-01-09 12:07:33 +0100

    fix(app): adjust env import

--- apps/rpk-client/src/main.server.ts
+++ apps/rpk-client/src/main.server.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
 
-import { environment } from '@rampike/shared/environments';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
