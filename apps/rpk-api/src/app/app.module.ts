878162757c9ff332082dc2f0f3bd4dde9ae0a2c6
Kevin Döring <k.doering.dt@gmail.com>
2021-01-09 12:00:09 +0100

    feat(db): add typeorm root config

--- apps/rpk-api/src/app/app.module.ts
+++ apps/rpk-api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '5dp4sa3kd2dt',
+    database: 'rpk-api',
+    entities: [],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
