5a2b1c744219dc4bc42e17827999f1683b4a7270
Kevin Döring <k.doering.dt@gmail.com>
2021-01-16 00:31:48 +0100

    add release step

--- .deploy/release-api.sh
+++ .deploy/release-api.sh
@@ -4,12 +4,21 @@ if [ -f ~/release/.env ]; then
     . ~/release/.env
 fi
 
-if [ -f ~/apx/manifests/$CI_PROJECT_NAME/package.json ]; then
+if [ -f ~/apx/manifests/$CI_PROJECT_NAME/kustomization/kustomization.yaml ]; then
   cd ~/apx/manifests/$CI_PROJECT_NAME
   git pull
 else
   mkdir -p ~/apx/manifests
   cd ~/apx/manifests
   git clone $MANIFEST_REPOSITORY
-  cd ./$CI_PROJECT_NAME
 fi
+
+cd ~/apx/manifests/$CI_PROJECT_NAME/kustomization
+kustomize edit set image $DOCKER_USER/$APP_NAME=$DOCKER_USER/$APP_NAME:$VERSION
+
+cd ~/apx/manifests/$CI_PROJECT_NAME
+git add kustomization.yaml
+git commit -m "[ci] update image version"
+git push
+
+
